16cf8b3aa0b266bbb3dcdb153b2efaa0330a43a0
David Ritchey <ritchey0713@gmail.com>
2018-03-23 13:37:28 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array_friends.push("John")
+  array.push("wow")
 end
 
 def add_element_to_start_of_array(array, element)

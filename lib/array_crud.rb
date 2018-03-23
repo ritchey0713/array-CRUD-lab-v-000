78c8ce460bcb4a875cb97f7d63e20134c1990e42
David Ritchey <ritchey0713@gmail.com>
2018-03-23 13:32:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array_friends = ["Derek", "Chris", "Samee", "Austin", "John"]
+  array_friends = ["Derek", "Chris", "Samee", "Austin"]
   
 end
 
